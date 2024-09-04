373c87395213f0df10dfb7a016d06610b989abbc
Ingrid <ingrid_anjos@hotmail.com>
2024-09-04 16:16:00 -0300

    feat:create initial chat structure

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { ArrowLeftComponent } from "../../icons/arrow-left/arrow-left.component";
+import { MuseumComponent } from "../../icons/museum/museum.component";
+import { ChatSuggestionsComponent } from '../../components/chat-suggestions/chat-suggestions.component';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [
+    ArrowLeftComponent, 
+    MuseumComponent,
+    ChatSuggestionsComponent
+  ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
-
+ 
 }
